51f9208684b85f7e47cfa593f09c7d20fdb1b73f
DeadL <ihor.chumachenko@inhubber.com>
2021-04-28 15:35:02 +0300

    4

--- script/script.js
+++ script/script.js
@@ -1,5 +1,52 @@
 $(document).ready(function () {
-    $(".nav-item.dropdown > a").click(function (e) {
+    mainMenu()
+    navWithMoreList()
+    switchListItem($('.listWithMore'));
+    $(window).resize(function () {
+        switchListItem($('.listWithMore'));
+    });
+
+    $('.slider-links').slick({
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+});
+
+
+const switchListItem = (mainBlock) => {
+    const mainBlockWidth = mainBlock.width();
+    const listWidth = mainBlock.children('ul').width()
+    const mainList = mainBlock.children('ul')
+    const lastMainElement = mainBlock.children('ul').children('ul li:nth-last-child(2)')
+    const dropDownContainer = mainBlock.find('ul > li:last-child .moreContainer > ul')
+    const firstSecondElement = dropDownContainer.children('li:nth-child(2)')
+    if (listWidth > mainBlockWidth) {
+        dropDownContainer.append(lastMainElement)
+        switchListItem(mainBlock)
+    }
+
+    /*    mainList.children('li:last-child').before(firstSecondElement)
+        switchListItem(mainBlock)*/
+}
+
+const navWithMoreList = () => {
+    $('.moreBtn').click(function (e) {
+        $(this).parents('.moreContainer').toggleClass('open')
+    })
+
+    $(document).on('click', function (e) {
+        e.stopPropagation();
+        if (!$(e.target).hasClass('show') && $('.moreContainer.open').find(e.target).length === 0) {
+            $('.moreContainer.open').removeClass('open');
+        }
+    });
+}
+
+const mainMenu = () => {
+    $(".nav-item.dropdown-container > a").click(function (e) {
         e.stopPropagation()
         let activeBlocks = $(this).parents('.navbar-nav').find('.show')
         if (activeBlocks.length > 1 || $(activeBlocks[0]).children('a').children('span:first-child').text() !== $(this).parent().children('a').children('span:first-child').text()) {
@@ -10,35 +57,35 @@ $(document).ready(function () {
     $(".submenu-button").click(function (e) {
         e.stopPropagation()
         $(this).parent().toggleClass('submenu-open');
-        $(this).parents('.dropdown').toggleClass('submenu-open');
+        $(this).parents('.dropdown-container').toggleClass('submenu-open');
     });
     $(".dropdown-back").click(function (e) {
         e.stopPropagation()
         $(this).parents('.submenu-open.nav-item').toggleClass('submenu-open');
-        $(this).parents('.submenu-open.nav-item').parents('.dropdown').toggleClass('submenu-open');
+        $(this).parents('.submenu-open.nav-item').parents('.dropdown-container').toggleClass('submenu-open');
     });
 
     $(document).on('click', function (e) {
         e.stopPropagation();
         if (!$(e.target).hasClass('submenu-open') && $('.submenu-open').find(e.target).length === 0) {
             $('.submenu-button').parent().removeClass('submenu-open');
-            $('.submenu-button').parent().parents('.dropdown').removeClass('submenu-open');
+            $('.submenu-button').parent().parents('.dropdown-container').removeClass('submenu-open');
         }
     });
 
     $(document).on('click', function (e) {
         e.stopPropagation();
-        if (!$(e.target).hasClass('show') && $('.dropdown.show').find(e.target).length === 0) {
-            $('.dropdown.show').removeClass('show');
+        if (!$(e.target).hasClass('show') && $('.dropdown-container.show').find(e.target).length === 0) {
+            $('.dropdown-container.show').removeClass('show');
         }
     });
+}
+
+const prevNews = (e) => {
+    $(e.target).parents('.filter').children('ul').find('a.active').parent().prev().children('a').click()
+}
 
 
-    $('.slider-links').slick({
-        dots: false,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 5,
-        slidesToScroll: 3
-    });
-});
+const nextNews = (e) => {
+    $(e.target).parents('.filter').children('ul').find('a.active').parent().next().children('a').click()
+}

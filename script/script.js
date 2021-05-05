06765387917104ba9006207e85b5893119df3506
Tanya <tanyayv25@gmail.com>
2021-05-05 12:18:40 +0300

    05052021 (slider)

--- script/script.js
+++ script/script.js
@@ -13,7 +13,44 @@ $(document).ready(function () {
         pauseOnFocus:   false,
         pauseOnHover:   false,
         slidesToShow:   6,
-        slidesToScroll: 5
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1815,
+                settings: {
+                    slidesToShow:   5,
+                    slidesToScroll: 4,
+                }
+            },
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow:   4,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow:   3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 860,
+                settings: {
+                    slidesToShow:   2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow:   1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     $(".item-eye").click(function() {
